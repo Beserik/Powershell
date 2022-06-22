234b3c92c918de037af1ae924427218ddb8ec9c8
Beserik <56308465+Beserik@users.noreply.github.com>
2022-06-22 10:59:28 +0200

    Add files via upload

---
+++ DetectionRule_InternetExplorer.ps1
@@ -0,0 +1,10 @@
+﻿ $Windowsfeature = Get-WindowsOptionalFeature -FeatureName Internet-Explorer-Optional-amd64 –Online | Where-Object {$_.State –eq 'Disabled','DisablePending'} 
+if ($Windowsfeature)
+ {
+        Write-host "Windows Feature Pending or Disabled"
+        Exit 1}
+ 
+else { 
+Write-Host "Windows Feature not found" 
+Exit 0
+}
---
+++ Office_update_channel.ps1
@@ -0,0 +1,17 @@
+﻿$path = "HKLM:\SOFTWARE\Microsoft\Office\ClickToRun\Configuration"
+$value = "UpdateChannel"
+$data = "http://officecdn.microsoft.com/pr/55336b82-a18d-4dd6-b5f6-9e5095c314a6"
+
+if (Test-Path -Path $path) {
+    (New-ItemProperty -Path $path -Name $value -Value $data -PropertyType STRING -Force)
+}
+
+$path = "HKLM:\SOFTWARE\Microsoft\Office\ClickToRun\Configuration"
+$value = "CDNBaseUrl"
+$data = "http://officecdn.microsoft.com/pr/55336b82-a18d-4dd6-b5f6-9e5095c314a6"
+
+if (Test-Path -Path $path) {
+    (New-ItemProperty -Path $path -Name $value -Value $data -PropertyType STRING -Force)
+}
+
+& “C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe” /update user displaylevel=false forceappshutdown=false /q
